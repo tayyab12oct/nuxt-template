1fbd8bb057f57021a3945ed4159854899910afd7
Tayyab Mughal <tayyab.8563@gmail.com>
2022-03-15 18:09:54 +0500

    fix error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,69 +1,69 @@
-import{defineNuxtConfig}from'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
 
-    publicRuntimeConfig: {
-      APP_TOKEN: process.env.APP_TOKEN,
-      CTF_SPACE_ID: process.env.CTF_SPACE_ID,
-      CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
-    },
+  // Target: https://go.nuxtjs.dev/config-target
+  target: "static",
+  // publicRuntimeConfig: {
+  //     APP_TOKEN: process.env.APP_TOKEN,
+  //     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+  //     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+  // },
 
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: process.env.APP_NAME,
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-                hid: 'description',
-                name: 'description',
-                content: 'abc'
-            },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-    },
-
-    buildModules: [
-        '@nuxtjs/pwa',
-        //'@nuxt/image',
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: process.env.APP_NAME,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "abc",
+      },
+      { name: "format-detection", content: "telephone=no" },
     ],
+  },
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://sitemap.nuxtjs.org/guide/setup
-        '@nuxtjs/sitemap',
-        '@intlify/nuxt3',
-    ],
+  buildModules: [
+    "@nuxtjs/pwa",
+    //'@nuxt/image',
+  ],
 
-    // config for `@intlify/nuxt3`
-    intlify: {
-      localeDir: 'lang',
-      vueI18n: {
-        defaultLocale: 'en',
-        locale: 'en',
-      }
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://sitemap.nuxtjs.org/guide/setup
+    "@nuxtjs/sitemap",
+    "@intlify/nuxt3",
+  ],
+
+  // config for `@intlify/nuxt3`
+  intlify: {
+    localeDir: "lang",
+    vueI18n: {
+      defaultLocale: "en",
+      locale: "en",
     },
+  },
+  image: {},
 
-    image: {},
+  sitemap: {
+    hostname: process.env.APP_URL,
+    gzip: true,
+  },
 
-    sitemap: {
-        hostname: process.env.APP_URL,
-        gzip: true
-    },
-
-    build: {
-      transpile: ['@heroicons/vue'],
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                },
-            },
+  build: {
+    transpile: ["@heroicons/vue"],
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
         },
+      },
     },
-    css: [
-        "~/assets/css/tailwind.css"
-    ],
-})
+  },
+  css: ["~/assets/css/tailwind.css"],
+});
